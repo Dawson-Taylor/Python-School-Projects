e20a5080897e9293f836c89e62ea01b76a31f521
Dawson <71582822+FeignedFIlms@users.noreply.github.com>
2021-11-18 12:29:43 -0500

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,14 +1,29 @@
 import random
 
 
-userGuess = int(input("Guess a number between 1 and 10: "))
+# Generate a random number between one and ten
+# Set number of guesses to 0 
 randomNum = random.randint(0, 10)
+numberOfGuesses = 0 
 
-while randomNum != randomNum
-  if userGuess > randomNum
-    print("The number is less than", userGuess + '.', "Try again.")
-  elif userGuess < randomNum
-    print("The number is greater than", userGuess + '.', "Try again.")
-  else
+# WHILE the random number is not equal to random number 
+  # Get user input for a number that needs to be guessed
+  # IF the user guessed greater than the generated random number
+    # print The number is lower than the guessed number
+  # ELSE IF the user guessed lower than than generated random number
+    # print The number is higher than the guessed number
+  # ELSE 
+    # The user guessed number is not lower than or greater than the generated random number break the IF loop
+while userGuess != randomNum
+  numberOfGuesses += 1
+  userGuess = int(input("Guess a number between 1 and 10: "))
+  if userGuess > randomNum:
+    print("The number is lower than", userGuess + '.', "Try again.")
+    continue
+  elif userGuess < randomNum:
+    print("The number is higher than", userGuess + '.', "Try again.")
+    continue
+  else:
     break
 print("You have guessed the right number!!!")
+print("It took you this many", numberOfGuesses, "guesses")
