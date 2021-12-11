de1b0eee835e1a73dd1462bce78eda68c166c21e
Dawson-Taylor <dwsntlr@gmail.com>
2021-12-10 23:40:57 -0500

    * added comments to most lines

--- NeanderthalTextAventureGame.py
+++ NeanderthalTextAventureGame.py
@@ -8,8 +8,8 @@ def showInstructions():
     print('You can choose to restart the game at any time by typing "restart" or "Restart"')
 
 
+# defines a function to restart the game after you lose or win
 def restart():
-
     restartGame = input("Would you like to play again (yes or y/no or n)? ")
     if restartGame == "yes" or restartGame == "y":
         main()
@@ -19,6 +19,7 @@ def restart():
     restart()
 
 
+# Sets the player status and is called from the main function
 def playerStatus(currentRoom, Inventory, rooms):
     print('   -------------------------------------------')
     print('You are in the {}'.format(currentRoom))
@@ -27,6 +28,7 @@ def playerStatus(currentRoom, Inventory, rooms):
     print('   -------------------------------------------')
 
 
+# Sets the currentRoom to an empty string
 currentRoom = ''
 # define the player inventory which starts empty.
 Inventory = set()
@@ -34,16 +36,19 @@ Inventory = set()
 
 def main():
     rooms = {
-        'Grasslands': {'go South': 'Caves', 'go North': 'Beach', 'go East': 'Icy Forest', 'go West': 'Forest', 'Item': 'None'},
+        'Grasslands': {'go South': 'Caves', 'go North': 'Beach', 'go East': 'Icy Forest', 'go West': 'Forest',
+                       'Item': 'None'},
         'Caves': {'go North': 'Grasslands', 'go West': 'Rocky Plateau', 'Item': 'Cave Moss Helmet'},
         'Beach': {'go South': 'Grasslands', 'go East': 'Camp', 'go West': 'Mountains', 'Item': 'Seashell Boots'},
         'Icy Forest': {'go North': 'Camp', 'go East': 'Mammoth Fields', 'go West': 'Grasslands', 'Item': 'Icy Shield'},
-        'Forest': {'go North': 'Mountains', 'go East': 'Grasslands', 'go South': 'Rocky Plateau', 'Item': 'Leafy Chestplate'},
+        'Forest': {'go North': 'Mountains', 'go East': 'Grasslands', 'go South': 'Rocky Plateau',
+                   'Item': 'Leafy Chestplate'},
         'Mountains': {'go South': 'Forest', 'go East': 'Beach', 'Item': 'Rock Club'},
         'Rocky Plateau': {'go North': 'Forest', 'go East': 'Caves', 'Item': 'Rocky Leggings'},
         'Camp': {'go West': 'Beach', 'go South': 'Icy Forest', 'Item': 'Fire Stick'},
         'Mammoth Fields': {'go West': 'Icy Forest', 'Item': 'Mammoth'}
     }
+    # Sets the player in the starter room
     currentRoom = 'Grasslands'
 
     # Show player the game instructions
@@ -51,20 +56,26 @@ def main():
 
     # loop forever
     while True:
-
+        # Lets the player know the status of the game
         playerStatus(currentRoom, Inventory, rooms)
+        # Gets input from the player for movement, getting items, or fighting the boss
         playerMove = input('Enter your move: ')
+        # Checks if the player input is exit to exit the program
         if playerMove in ['Exit', 'exit']:
             currentRoom = 'Exit'
             print('Play again soon!')
             exit()
-
-        item = rooms[currentRoom].get('Item')  # get the item present, or None if there isn't one
+        # Get the item present, or None if there isn't one
+        item = rooms[currentRoom].get('Item')
+        # Gets input from playerMovement to get an item if there is an item in the room
+        # also tells you if you already have the item
         if item != 'None' and playerMove == 'get ' + item:
             if item in Inventory:
                 print('You already have this item in your inventory!')
             else:
                 Inventory.add(item)
+        # Gets input from playerMovement to fight the boss and checks if the player
+        # has enough items to win or lose
         if item == 'Mammoth' and playerMove == 'Fight ' + item:
             if item == 7:
                 print('You have beaten the great mammoth. Your people thank you.')
@@ -74,11 +85,12 @@ def main():
                 print('The mammoth has beaten you. It heads towards your camp.')
                 print('Thank you for playing.')
                 restart()
+        # Allows the player to restart the program at any time
         if playerMove == 'Restart' or 'restart':
             main()
         else:
             continue
-
+        # Gets input for movement, items, or to fight the boss
         try:
             currentRoom = rooms[currentRoom][playerMove]
         except KeyError:
