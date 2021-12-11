92e4a9ca0ead7a707cff4a5db9b6797dd0e83184
Dawson-Taylor <dwsntlr@gmail.com>
2021-12-10 22:59:02 -0500

    * Added a restart function. * Added the boss fight feature.

--- NeanderthalTextAventureGame.py
+++ NeanderthalTextAventureGame.py
@@ -4,7 +4,18 @@ def showInstructions():
     print('Collect all 7 Items and defeat the mammoth boss at the end to win.')
     print('Movement commands: go South, go North, go East, go West')
     print("To add item to inventory: get 'item name'")
-    print('To fight the boss type: "Fight Boss"')
+    print('To fight the boss type: "Fight Mammoth"')
+
+
+def restart():
+
+    restartGame = input("Would you like to play again?")
+    if restartGame == "yes" or restartGame == "y":
+        main()
+    if restartGame == "n" or restartGame == "no":
+        print("Thank you for playing. Goodbye.")
+        exit()
+    restart()
 
 
 def player_stat(currentRoom, Inventory, rooms):
@@ -19,18 +30,19 @@ currentRoom = ''
 # define the player inventory which starts empty.
 Inventory = set()
 
+
 def main():
     rooms = {
-        'Grasslands': {'go South': 'Caves', 'go North': 'Beach', 'go East': 'Icy Forest', 'go West': 'Forest', 'Item': 'None'},
-        'Caves': {'go North': 'Grasslands', 'go West': 'Rocky Plateau', 'Item': 'Cave Moss Helmet'},
-        'Beach': {'go South': 'Grasslands', 'go East': 'Camp', 'go West': 'Mountains', 'Item': 'Seashell Boots'},
-        'Icy Forest': {'go North': 'Camp', 'go East': 'Mammoth Fields', 'go West': 'Grasslands', 'Item': 'Icy Shield'},
-        'Forest': {'go North': 'Mountains', 'go East': 'Grasslands', 'go South': 'Rocky Plateau',
+        'Grasslands': {'South': 'Caves', 'North': 'Beach', 'East': 'Icy Forest', 'West': 'Forest', 'Item': 'None'},
+        'Caves': {'North': 'Grasslands', 'West': 'Rocky Plateau', 'Item': 'Cave Moss Helmet'},
+        'Beach': {'South': 'Grasslands', 'East': 'Camp', 'West': 'Mountains', 'Item': 'Seashell Boots'},
+        'Icy Forest': {'North': 'Camp', 'East': 'Mammoth Fields', 'West': 'Grasslands', 'Item': 'Icy Shield'},
+        'Forest': {'North': 'Mountains', 'East': 'Grasslands', 'South': 'Rocky Plateau',
                    'Item': 'Leafy Chestplate'},
-        'Mountains': {'go South': 'Forest', 'go East': 'Beach', 'Item': 'Rock Club'},
-        'Rocky Plateau': {'go North': 'Forest', 'go East': 'Caves', 'Item': 'Rocky Leggings'},
-        'Camp': {'go West': 'Beach', 'go South': 'Icy Forest', 'Item': 'Fire Stick'},
-        'Mammoth Fields': {'go West': 'Icy Forest', 'Fight Boss': 'Mammoth'}
+        'Mountains': {'South': 'Forest', 'East': 'Beach', 'Item': 'Rock Club'},
+        'Rocky Plateau': {'North': 'Forest', 'East': 'Caves', 'Item': 'Rocky Leggings'},
+        'Camp': {'West': 'Beach', 'South': 'Icy Forest', 'Item': 'Fire Stick'},
+        'Mammoth Fields': {'West': 'Icy Forest', 'Item': 'Mammoth'}
     }
     currentRoom = 'Grasslands'
 
@@ -53,12 +65,21 @@ def main():
                 print('You already have this item in your inventory!')
             else:
                 Inventory.add(item)
+        if item == 'Mammoth' and playerMove == 'Fight ' + item:
+            if item == 7:
+                print('You have beaten the great mammoth. Your people thank you.')
+                print('Thank you for playing')
+                restart()
+            else:
+                print('The mammoth has beaten you. It heads towards your camp.')
+                print('Thank you for playing.')
+                restart()
+
         try:
             currentRoom = rooms[currentRoom][playerMove]
         except KeyError:
-            print("invalid move")
+            print("You can not go that way")
             continue
-
 
 
 main()
